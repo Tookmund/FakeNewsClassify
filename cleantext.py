fea51e806737d04f92bcc05093b3e48fe883916e
Jacob Adams <jacob@tookmund.com>
2019-03-28 12:35:42 -0400

    cleantext: Split text into words

--- cleantext.py
+++ cleantext.py
@@ -1,5 +1,9 @@
 from nltk.corpus import stopwords
 stop = stopwords.words('english')
 
+def toWords(text):
+    return [w for w in text.split()]
+
 def cleanText(text):
+    text = toWords(text)
     return [w for w in text if w.isalpha() and w not in stop]

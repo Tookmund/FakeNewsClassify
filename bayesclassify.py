d2a46906cc7037c10c49af86666fefdcc903ad97
Jacob Adams <jacob@tookmund.com>
2019-03-28 12:28:31 -0400

    bayesclassify: fix copy/paste errors
    Code borrowed from lecture

--- bayesclassify.py
+++ bayesclassify.py
@@ -13,18 +13,18 @@ wordfeatures = list(allwords)[:2000]
 def documentFeatures(document):
     documentWords = set(document)
     features = {}
-    for word in wordfeatures
+    for word in wordfeatures:
         features['contains({})'.format(word)] = (word in documentWords)
     return features
 
-featuresets = [(document_features(d[0]), d[1]) for d in readFNC.data]
+featuresets = [(documentFeatures(d[0]), d[1]) for d in readFNC.data]
 
 TESTNUM = 100
 testSet = featuresets[:TESTNUM]
 trainSet = featuresets[TESTNUM:]
 
-classifier = nltk.NaiveBayesClassifier.train(train_set)
+classifier = nltk.NaiveBayesClassifier.train(trainSet)
 
-print(nltk.classify.accuracy(classifier, test_set))
+print(nltk.classify.accuracy(classifier, testSet))
 
 classifier.show_most_informative_features(50)

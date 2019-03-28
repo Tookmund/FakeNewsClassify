bd2c249a821462299895ea47906dd8904ce43da4
Jacob Adams <jacob@tookmund.com>
2019-03-28 11:11:03 -0400

    Port to python3

--- readFNC.py
+++ readFNC.py
@@ -10,12 +10,12 @@ with open('data/FakeNewsCorpus.csv', 'rU') as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
         if len(row) < 3:
-            print row
+            print(row)
             continue
         if row[3] == 'fake':
             totalFake += 1
         elif row[3] == 'reliable':
             totalReal += 1
 
-    print "Real: %d" % totalReal
-    print "Fake: %d" % totalFake
+    print("Real: %d" % totalReal)
+    print("Fake: %d" % totalFake)

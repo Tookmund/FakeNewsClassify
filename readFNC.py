7eddd2eb0322856dc54aff66180b8f0ec873862d
Jacob Adams <jacob@tookmund.com>
2019-03-28 11:53:22 -0400

    readFNC: Collect all data into one list

--- readFNC.py
+++ readFNC.py
@@ -3,8 +3,10 @@ import sys
 
 csv.field_size_limit(sys.maxsize)
 
-real = []
-fake = []
+totalReal = 0
+totalFake = 0
+
+data = []
 
 with open('data/FakeNewsCorpus.csv', 'rU') as csvfile:
     reader = csv.reader(csvfile, dialect='excel')
@@ -12,9 +14,12 @@ with open('data/FakeNewsCorpus.csv', 'rU') as csvfile:
         if len(row) < 16:
             continue
         if row[3] == 'fake':
-            fake.append(row)
+            totalReal += 1
         elif row[3] == 'reliable':
-            real.append(row)
+            totalFake += 1
+        else:
+            continue
+        data.append((row[5], row[3]))
 
-    print("Real: %d" % len(real))
-    print("Fake: %d" % len(fake))
+    print("Real: %d" % totalReal)
+    print("Fake: %d" % totalFake)

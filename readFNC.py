9e03f999aa927c52d7fa4e6323ef1fe79476431b
Jacob Adams <jacob@tookmund.com>
2019-03-28 00:48:31 -0400

    readFNC: Print broken rows

--- readFNC.py
+++ readFNC.py
@@ -6,6 +6,8 @@ totalFake = 0
 with open('data/FakeNewsCorpus.csv', 'rU') as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
+        if len(row) < 3:
+            print row
         if row[3] == 'fake':
             totalFake += 1
         elif row[3] == 'reliable':

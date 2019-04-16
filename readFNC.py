a5c8c10b5f5a7f9408e54d63fb6d2094742d66c6
Jacob Adams <jacob@tookmund.com>
2019-04-16 12:34:20 -0400

    readFNC: Cache results

--- readFNC.py
+++ readFNC.py
@@ -3,6 +3,7 @@ import sys
 import random
 from urllib.parse import urlparse
 import re
+import pickle
 
 import cleantext
 
@@ -27,48 +28,66 @@ dontuse = [
 data = []
 source = {}
 
-host = re.compile(r"(w+\S*\.)*([^\.\s]+\.[^\.\s]+)")
-with open('data/FakeNewsCorpus.csv', 'rU', newline='') as csvfile:
-    reader = csv.reader(csvfile, dialect='excel')
-    for row in reader:
-        if len(row) < 16:
-            continue
-
-        loc = urlparse(row[4]).netloc
-        if loc == '':
-            continue
-        loc = host.match(loc).group(2)
-        if loc in dontuse:
-            continue
-        if loc in source:
-            if source[loc] >= PERSRC:
+def loaddata():
+    global total
+    global totalReal
+    global totalFake
+    host = re.compile(r"(w+\S*\.)*([^\.\s]+\.[^\.\s]+)")
+    with open('data/FakeNewsCorpus.csv', 'rU', newline='') as csvfile:
+        reader = csv.reader(csvfile, dialect='excel')
+        for row in reader:
+            if len(row) < 16:
                 continue
-            source[loc] += 1
-        else:
-            source[loc] = 1
 
-        if row[3] == 'fake':
-            if totalFake >= EACH:
+            loc = urlparse(row[4]).netloc
+            if loc == '':
                 continue
-            totalFake += 1
-        elif row[3] == 'reliable':
-            if totalReal >= EACH:
+            loc = host.match(loc).group(2)
+            if loc in dontuse:
                 continue
-            totalReal += 1
-        else:
-            continue
+            if loc in source:
+                if source[loc] >= PERSRC:
+                    continue
+                source[loc] += 1
+            else:
+                source[loc] = 1
 
-        ct = cleantext.cleanText(row[5])
-        data.append([ct, row[3]])
-        print(row[1], loc)
-        total += 1
-        if totalFake >= EACH and totalReal >= EACH:
-            break
+            if row[3] == 'fake':
+                if totalFake >= EACH:
+                    continue
+                totalFake += 1
+            elif row[3] == 'reliable':
+                if totalReal >= EACH:
+                    continue
+                totalReal += 1
+            else:
+                continue
 
-for s, n in source.items():
-    print(s, ":", n)
+            ct = cleantext.cleanText(row[5])
+            data.append([ct, row[3]])
+            print(row[1], loc)
+            total += 1
+            if totalFake >= EACH and totalReal >= EACH:
+                break
 
+    for s, n in source.items():
+        print(s, ":", n)
+
+    with open("data/fncdata.pkl", "wb") as p:
+        pickle.dump([total, totalFake, totalReal, data], p)
+
+try:
+    with open("data/fncdata.pkl", 'rb') as p:
+        ret = pickle.load(p)
+        total = ret[0]
+        totalFake = ret[1]
+        totalReal = ret[2]
+        data = ret[3]
+
+except:
+    loaddata()
+
+random.shuffle(data)
 print("Real: %d" % totalReal)
 print("Fake: %d" % totalFake)
 print("Total: %d" % total)
-random.shuffle(data)

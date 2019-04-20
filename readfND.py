261c707504ca0a20f637a06d1e39ec15ee70e3f1
Jacob Adams <jacob@tookmund.com>
2019-04-20 15:13:20 -0400

    readfND: Add Fake News Dataset

---
+++ readfND.py
@@ -0,0 +1,27 @@
+import os
+
+import cleantext
+
+DATASET="data/fakeNewsDatasets/fakeNewsDataset/"
+data = []
+
+totalReal = 0
+totalFake = 0
+total = 0
+
+for n in os.listdir(DATASET+"/fake"):
+    with open(DATASET+"/fake/"+n) as f:
+        ct = cleantext.cleanText(f.read())
+        data.append([ct, "fake"])
+        totalFake += 1
+for n in os.listdir(DATASET+"/legit"):
+    with open(DATASET+"/legit/"+n) as f:
+        ct = cleantext.cleanText(f.read())
+        data.append([ct, "legit"])
+        totalReal += 1
+
+total = totalFake + totalReal
+
+print("Real: %d" % totalReal)
+print("Fake: %d" % totalFake)
+print("Total: %d" % total)

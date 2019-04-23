3854431a9b91ea37962f15ee68568dfe075899e1
Jacob Adams <jacob@tookmund.com>
2019-04-23 00:14:46 -0400

    readfND: Support loaddata

--- readfND.py
+++ readfND.py
@@ -10,23 +10,37 @@ totalReal = 0
 totalFake = 0
 total = 0
 
-for n in os.listdir(DATASET+"/fake"):
-    with open(DATASET+"/fake/"+n) as f:
-        ct = cleantext.cleanText(f.read())
-        data.append([ct, "fake"])
-        totalFake += 1
-for n in os.listdir(DATASET+"/legit"):
-    with open(DATASET+"/legit/"+n) as f:
-        ct = cleantext.cleanText(f.read())
-        data.append([ct, "reliable"])
-        totalReal += 1
+def loaddata(cleaned=True):
+    global total
+    global totalReal
+    global totalFake
+    global data
+    data = []
+    total = 0
+    totalReal = 0
+    totalFake = 0
+    for n in os.listdir(DATASET+"/fake"):
+        with open(DATASET+"/fake/"+n) as f:
+            if cleaned:
+                ct = cleantext.cleanText(f.read())
+            else:
+                ct = f.read()
+            data.append([ct, "fake"])
+            totalFake += 1
+    for n in os.listdir(DATASET+"/legit"):
+        with open(DATASET+"/legit/"+n) as f:
+            if cleaned:
+                ct = cleantext.cleanText(f.read())
+            else:
+                ct = f.read()
+            data.append([ct, "reliable"])
+            totalReal += 1
 
-random.shuffle(data)
-total = totalFake + totalReal
+    random.shuffle(data)
+    total = totalFake + totalReal
 
-print("Real: %d" % totalReal)
-print("Fake: %d" % totalFake)
-print("Total: %d" % total)
-# for compatability
-def loaddata(b):
-    pass
+    print("Real: %d" % totalReal)
+    print("Fake: %d" % totalFake)
+    print("Total: %d" % total)
+
+loaddata()
